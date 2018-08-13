043dc1bef17e5217421c87ccc6af5732557ffefe
Jon Black <jblack@bol.com>
2018-08-13 15:33:17 +0200

    Work with new CSV format

--- convert.py
+++ convert.py
@@ -1,49 +1,78 @@
-from datetime import datetime
+import argparse
 import csv
+import sys
+
+from datetime import datetime
+
+def convert(rabobank_csvfile, output_csvfile):
+    with rabobank_csvfile as f, output_csvfile as out:
+        fieldnames = (
+            'IBAN/BBAN',
+            'Munt',
+            'BIC',
+            'Volgnr',
+            'Datum',
+            'Rentedatum',
+            'Bedrag',
+            'Saldo na trn',
+            'Tegenrekening IBAN/BBAN',
+            'Naam tegenpartij',
+            'Naam uiteindelijke partij',
+            'Naam initi�rende partij',
+            'BIC tegenpartij',
+            'Code',
+            'Batch ID',
+            'Transactiereferentie',
+            'Machtigingskenmerk',
+            'Incassant ID',
+            'Betalingskenmerk',
+            'Omschrijving-1',
+            'Omschrijving-2',
+            'Omschrijving-3',
+            'Reden retour',
+            'Oorspr bedrag',
+            'Oorspr munt',
+            'Koers'
+        )
+
+        reader = csv.DictReader(f, fieldnames=fieldnames)
+        next(reader, None) # skip header
+        writer = csv.writer(out)
+        writer.writerow(('Date', 'Payee', 'Category', 'Memo', 'Outflow', 'Inflow'))
+        for row in reader:
+            date = convert_date(row['Datum'])
+            payee = row['Naam tegenpartij']
+            memo = ''.join((row['Omschrijving-1'], row['Omschrijving-2'], row['Omschrijving-3']))
+
+            if row['Bedrag'][0] == '+':
+                outflow = 0
+                inflow = row['Bedrag'][1:].replace(',', '.')
+            else:
+                outflow = row['Bedrag'][1:].replace(',', '.')
+                inflow = 0
+
+            writer.writerow((date, payee, '', memo, outflow, inflow))
 
 
-with open('transactions.csv', 'r') as f, open('output.csv', 'w') as out:
-    fieldnames = (
-        'iban',
-        'currency',
-        'interest_date',
-        'by_af_code',
-        'amount',
-        'against_bill',
-        'to_name',
-        'book_date',
-        'book_code',
-        'filler',
-        'desc_1',
-        'desc_2',
-        'desc_3',
-        'desc_4',
-        'desc_5',
-        'desc_6',
-        'end_to_end_id',
-        'id_to_account',
-        'mandate_id',
+def convert_date(date):
+    date = datetime.strptime(date, '%Y-%m-%d')
+    return date.strftime('%d/%m/%Y')
 
+
+def parse_cli_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        'rabobank_csvfile',
+        type=argparse.FileType('r', encoding='ISO-8859-1')
     )
-    reader = csv.DictReader(f, fieldnames=fieldnames)
-    writer = csv.writer(out)
+    parser.add_argument(
+        'output_csvfile',
+        nargs='?',
+        type=argparse.FileType('w', encoding='ISO-8859-1'),
+        default=sys.stdout
+    )
+    return parser.parse_args()
 
-    writer.writerow(('Date', 'Payee', 'Category', 'Memo', 'Outflow', 'Inflow'))
-    for row in reader:
-        date = datetime.strptime(row['interest_date'], '%Y%m%d')
-        date = date.strftime('%d/%m/%Y')
-        if row['to_name']:
-            payee = row['to_name']
-            memo = ''.join((row['desc_1'], row['desc_2'], row['desc_3'], row['desc_4'], row['desc_5']))
-        else:
-            payee = row['desc_1']
-            memo = ''.join((row['desc_2'], row['desc_3'], row['desc_4'], row['desc_5']))
-
-        if row['by_af_code'] == 'D':
-            outflow = row['amount']
-            inflow = ''
-        else:
-            inflow = row['amount']
-            outflow = ''
-
-        writer.writerow((date, payee, '', memo, outflow, inflow))
+if __name__ == '__main__':
+    args = parse_cli_args()
+    convert(args.rabobank_csvfile, args.output_csvfile)
